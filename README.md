a83b42ff017a8e082e5f9c2df75441c16ba4e62b
behai-nguyen <behai_nguyen@hotmail.com>
2024-05-12 00:43:13 +1000

    First post.

---
+++ README.md
@@ -0,0 +1,21 @@
+<!-- 11/05/2024. -->
+
+# fastapi_learning
+
+Documentation of my [FastAPI](https://fastapi.tiangolo.com/learn/) learning process. I document what I find necessary.
+
+Posts are listed in the [Related post(s)](#related-posts) section below. Each entry includes the link to the actual post, the ``git clone`` command for the target code revision, and an excerpt from the post.
+
+## Related post(s)
+
+1. [Python FastAPI: Some Further Studies on OAuth2 Security](https://behainguyen.wordpress.com/2024/05/11/python-fastapi-some-further-studies-on-oauth2-security/)
+
+```
+git clone -b v0.1.0 https://github.com/behai-nguyen/fastapi_learning.git
+```
+
+``FastAPI`` provides [excellent tutorials](https://fastapi.tiangolo.com/learn/) that thoroughly introduce the framework. Two sections on security, namely [Tutorial - User Guide Security](https://fastapi.tiangolo.com/tutorial/security/) and [Advanced User Guide Security](https://fastapi.tiangolo.com/advanced/security/), have sparked further questions, which we are discussing in this post. Hopefully, this discussion will lead to a better understanding of how ``FastAPI`` security works.
+
+## License
+[MIT license](http://www.opensource.org/licenses/mit-license.php)
+and the [GPL license](http://www.gnu.org/licenses/gpl.html).
---
+++ main.py
@@ -0,0 +1,106 @@
+# 26/04/2024.
+#
+# This code is from the official tutorial section:
+#     https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
+# 
+# To run:
+#     (venv) <path to venv Scripts/bin>/uvicorn main:app --host 0.0.0.0 --port 5000
+#
+# To access Swagger UI:
+#     http://localhost:5000/docs
+#
+
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+app = FastAPI()
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+"""
+When we create an instance of the OAuth2PasswordBearer class we pass 
+in the tokenUrl parameter. This parameter contains the URL that the 
+client (the frontend running in the user's browser) will use to send 
+the username and password in order to get a token.
+
+Here tokenUrl="token" refers to a relative URL token that we haven't 
+created yet. As it's a relative URL, it's equivalent to ./token.
+"""
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    print(f"get_current_user: <{token}>") # johndoe
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
