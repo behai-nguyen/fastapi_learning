e0b112105760ac54e9d6ff4331e026a17455928a
behai-nguyen <behai_nguyen@hotmail.com>
2024-06-11 21:14:34 +1000

    Added home page. Complete login flow. Added JSON /api/me and /api/login.
    Added some more constants.

--- src/fastapi_learning/common/consts.py
+++ src/fastapi_learning/common/consts.py
@@ -2,9 +2,19 @@
 15/05/2024.
 """
 
-LOGIN_PAGE_TITLE = "FastAPI Login"
+FORMAT_HEADER = "x-expected-format"
+
+LOGIN_PAGE_TITLE = "Learn FastAPI Login"
+HOME_PAGE_TITLE = "Learn FastAPI Home"
+ME_PAGE_TITLE = "Learn FastAPI My Info"
 
 INVALID_USERNAME_PASSWORD_MSG = "Incorrect username or password"
 NOT_LOGGED_IN_SESSION_MSG = "This session has not been logged in before"
 LOGGED_IN_SESSION_MSG = "Already logged in"
+
+NOT_AUTHENTICATED_MSG = "Not authenticated"
 INVALID_AUTH_CREDENTIALS_MSG = "Invalid authentication credentials"
+
+ALREADY_LOGGED_IN_MSG = "Already logged in. Redirect to home page"
+
+LOG_IN_CONTINUE_MSG = "Please log in to continue"
