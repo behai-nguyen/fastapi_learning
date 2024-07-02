0f8134c0e5414789dc253e0034fb59a26d741ec3
behai-nguyen <behai_nguyen@hotmail.com>
2024-07-02 16:42:24 +1000

    Implemented non-blocking logger using QueueHandler and QueueListener.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 #
 
 import os
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, Response
 
@@ -21,9 +22,31 @@ from fastapi.staticfiles import StaticFiles
 from starsessions import SessionAutoloadMiddleware, SessionMiddleware
 from starsessions.stores.redis import RedisStore
 
+from fastapi_learning.common.queue_logging import (
+    logger,
+    prepare_logging_and_start_listeners,
+    logging_stop_listeners,
+    RequestLoggingMiddleware,
+)
+
 from fastapi_learning.controllers import auth, admin
 
-app = FastAPI()
+prepare_logging_and_start_listeners()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger().info("fastapi_learning startup complete.")
+
+    yield
+
+    logger().info("fastapi_learning is shutting down...")
+    logger().info("Logging queue listener will stop listening...")
+
+    logging_stop_listeners()
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(RequestLoggingMiddleware)
 
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost")
 app.add_middleware(SessionAutoloadMiddleware)
