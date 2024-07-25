07e5ee651373bdee7a997119308d6d1356e422dc
behai-nguyen <behai_nguyen@hotmail.com>
2024-07-25 11:54:52 +1000

    SSL/HTTPS and CORS
    Updated to implement SSL/HTTPs for localhost and CORS.

--- main.py
+++ main.py
@@ -15,9 +15,13 @@
 import os
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi.staticfiles import StaticFiles
+
+from dotenv import load_dotenv
 
 from starsessions import SessionAutoloadMiddleware, SessionMiddleware
 from starsessions.stores.redis import RedisStore
@@ -48,9 +52,20 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(RequestLoggingMiddleware)
 
+load_dotenv( os.path.join(os.getcwd(), '.env') )
+
 REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost")
 app.add_middleware(SessionAutoloadMiddleware)
 app.add_middleware(SessionMiddleware, store=RedisStore(REDIS_URL), cookie_https_only=False)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.environ.get('ALLOW_ORIGINS').split(', '),
+    allow_credentials=os.environ.get('ALLOW_CREDENTIALS'),
+    allow_methods=os.environ.get('ALLOW_METHODS').split(', '),
+    allow_headers=os.environ.get('ALLOW_HEADERS').split(', '),
+    max_age=os.environ.get('MAX_AGE'),
+)
 
 app.mount("/static", StaticFiles(directory="src/fastapi_learning/static"), name="static")
 
@@ -63,3 +78,16 @@ app.include_router(admin.api_router)
 async def index(request: Request) -> Response | dict:
     from fastapi_learning.controllers.auth import login_page
     return await login_page(request)
+
+#
+# Remove the code block below to use the command:
+#
+#    (venv) <venv path> uvicorn main:app --host 0.0.0.0 --port 5000 --ssl-keyfile ./cert/key.pem --ssl-certfile ./cert/cert.pem
+#
+# The command to run with the below code block:
+#
+#    (venv) <venv path> python main.py
+#
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, \
+                ssl_keyfile="./cert/key.pem", ssl_certfile="./cert/cert.pem")
