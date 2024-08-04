eae629de113734eaf79934464448cbb2b5f8c335
behai-nguyen <behai_nguyen@hotmail.com>
2024-08-05 08:45:39 +1000

    Implements database employees: MySQL, PostgreSQL and MariaDB
    Completely rewritten:
        Removed all existing previous mock implementations
        Supports databases:
            - added class Employees(WriteCapableTable)
            - added class LoggedInEmployee(BaseModel)

--- src/fastapi_learning/models/employees.py
+++ src/fastapi_learning/models/employees.py
@@ -1,8 +1,8 @@
 """
 14/05/2024.
 
-Eventually it will be the "employees" table in "employees" MySQL 
-test database by Oracle Corporation: https://github.com/datacharmer/test_db.
+Implements the "employees" table in the "employees" MySQL test database 
+by the Oracle Corporation: https://github.com/datacharmer/test_db.
 
 See also: 
 
@@ -11,54 +11,52 @@ See also:
 2. https://behainguyen.wordpress.com/2024/01/14/rust-actix-web-endpoints-which-accept-both-application-x-www-form-urlencoded-and-application-json-content-types/#step-two-update-employees-table
 """
 
-from typing import Union
 from pydantic import BaseModel
 
-from argon2 import PasswordHasher
+from sqlalchemy import (
+    Column,
+    Integer,
+    Date,
+    String,
+)
 
-class User(BaseModel):
-    username: str
-    first_name: Union[str, None] = None
-    last_name: Union[str, None] = None
+from bh_apistatus.result_status import ResultStatus
+from bh_database.base_table import WriteCapableTable
 
-class UserInDB(User):
-    hashed_password: str
+class Employees(WriteCapableTable):
+    """
+    Implements the 'employees' table.
+    """
+    
+    __tablename__ = 'employees'
 
-"""
-Note on 'hashed_password' field value: it's Argon2 hashed version of 'password'.
-It was hashed by https://argon2.online/, using "Argon2id".
+    emp_no = Column(Integer, primary_key=True)
+    email = Column(String(255), nullable=False)
+    password = Column(String(100), nullable=False)
+    birth_date = Column(Date, nullable=False)
+    first_name = Column(String(14), nullable=False)
+    last_name = Column(String(16), nullable=False)
+    gender = Column(String(1), nullable=False)
+    hire_date = Column(Date, nullable=False)
 
-The Python implementation of Argon2 is https://pypi.org/project/argon2-cffi/,
-but we are not using this library yet.
-"""
-fake_users_db = {
-    "behai_nguyen@hotmail.com": {
-        "username": "behai_nguyen@hotmail.com",
-        "first_name": "Be Hai",
-        "last_name": "Doe",
-        "hashed_password": "$argon2id$v=19$m=16,t=2,p=1$b2szcWQ4a0tlTkFydUdOaw$7LX7WCYbItEMEwvH3yUxPA",
-    },
-    "pranav.furedi.10198@gmail.com": {
-        "username": "pranav.furedi.10198@gmail.com",
-        "first_name": "Pranav",
-        "last_name": "Furedi",
-        "hashed_password": "$argon2id$v=19$m=16,t=2,p=1$b2szcWQ4a0tlTkFydUdOaw$7LX7WCYbItEMEwvH3yUxPA",
-    },
-}
-
-def password_match(hashed_password, password: str):
-    try:
-        return PasswordHasher().verify(hashed_password, password)
-    except:
-        return False
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
+    def select_by_email(self, email: str) -> ResultStatus:
+        """
+        Note the usage of the single quotation marks '{0}': MySQL, PostgreSQL and
+        MariaDB accept this. PostgreSQL rejects double quotation mark ("{0}").
+        """
+        return self.run_select_sql(
+            "select * from employees where email = '{0}'".format(email), True)
+    
+class LoggedInEmployee(BaseModel):
+    """
+    See also https://docs.pydantic.dev/1.10/usage/models/. This is a Pydantic 
+    implementation: a SQLAlchemy class for database model and Pydantic model
+    for internal data representation.
+    """
+    email: str
+    password: str
+    birth_date: str
+    first_name: str
+    last_name: str
+    gender: str
+    hire_date: str
