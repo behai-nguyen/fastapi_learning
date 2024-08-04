32d12e56cae31bf74328efe8fd3dcc2776925f8f
behai-nguyen <behai_nguyen@hotmail.com>
2024-08-05 08:46:07 +1000

    Implements database employees: MySQL, PostgreSQL and MariaDB
    Added some more text constants.

--- src/fastapi_learning/common/consts.py
+++ src/fastapi_learning/common/consts.py
@@ -8,6 +8,11 @@ LOGIN_PAGE_TITLE = "Learn FastAPI Login"
 HOME_PAGE_TITLE = "Learn FastAPI Home"
 ME_PAGE_TITLE = "Learn FastAPI My Info"
 
+EMAIL_NO_MATCHING_MSG = 'No employee matches submitted email'
+
+LOGIN_ERROR_MSG = "Sorry, attempt to login did not succeed. Please contact support"
+BAD_LOGIN_MSG = "Bad username, bad password or some something else on my part. Please contact support"
+
 INVALID_USERNAME_PASSWORD_MSG = "Incorrect username or password"
 NOT_LOGGED_IN_SESSION_MSG = "This session has not been logged in before"
 LOGGED_IN_SESSION_MSG = "Already logged in"
