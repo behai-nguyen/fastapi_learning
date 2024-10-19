729d92e9315fe4b8c7e231c91ec32feb0b1a69b0
behai-nguyen <behai_nguyen@hotmail.com>
2024-10-19 22:07:58 +1100

    Optionally enable/disable UI elements based on logged in user assigned scopes.
    Added:
        - template method def enable_no_scopes_ui()
        - template method def has_required_scopes(...)
        - helper method def credentials_exception(...)
        - helper method def attempt_decoding_access_token(...)

--- src/fastapi_learning/controllers/__init__.py
+++ src/fastapi_learning/controllers/__init__.py
@@ -2,30 +2,92 @@
 14/05/2024.
 """
 
-from typing import Callable, Tuple
+from typing import (
+    Union, 
+    Callable, 
+    Tuple,
+)
 
 from mimetypes import types_map
 
 from fastapi.templating import Jinja2Templates
-from fastapi import Request, Response
+from fastapi import (
+    HTTPException, 
+    status,
+    Request, 
+    Response,
+)
 
 from fastapi.routing import APIRoute
 
-from fastapi_learning.common.consts import RESPONSE_FORMAT
+from fastapi_learning.common.queue_logging import logger
+from fastapi_learning import TokenData
+from fastapi_learning.common.consts import (
+    NOT_AUTHENTICATED_MSG,
+    RESPONSE_FORMAT,
+)
+from fastapi_learning.common.jwt_utils import decode_access_token
+from fastapi_learning.common.scope_utils import has_required_permissions
 
-def valid_logged_in_employee(data: dict):
+logger = logger()
+
+def valid_logged_in_employee(data: dict) -> bool:
     """
-    Used by templates. See ./templates/admin/me.html.
+    Template method. 
+    See ./templates/admin/me.html.
     """
 
     if 'email' in data:
         return True
     return False
 
+def enable_no_scopes_ui() -> bool:
+    """
+    Template method. 
+    Display UI elements which send requests to the server even though
+    the logged in user does not have sufficient scopes to run the 
+    endpoint handler methods.    
+    """
+
+    import os
+    from bh_utils.conversions import str_to_bool
+
+    return str_to_bool(os.environ.get('ENABLE_NO_SCOPES_UI'))
+
+def has_required_scopes(required_scopes: list, assigned_scopes: list) -> bool:
+    """
+    Template method. 
+    Check if all scopes in param assigned_scopes are in param 
+    required_scopes.
+    """
+    return has_required_permissions(required_scopes, assigned_scopes)
+
 templates = Jinja2Templates(directory="src/fastapi_learning/templates")
 # Added functions to be used by templates.
 # 
 templates.env.globals['valid_logged_in_employee'] = valid_logged_in_employee
+templates.env.globals['enable_no_scopes_ui'] = enable_no_scopes_ui
+templates.env.globals['has_required_scopes'] = has_required_scopes
+
+def credentials_exception(detail: str=NOT_AUTHENTICATED_MSG, 
+                          authenticate_value: str="Bearer") -> HTTPException:
+    return HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = detail,
+        headers = {"WWW-Authenticate": authenticate_value},
+    )
+
+def attempt_decoding_access_token(token: str) -> Union[TokenData, HTTPException]:
+    # OAuth2PasswordBearer auto_error has been set to False. 
+    # The return value is None instead of an HTTPException.
+    # This is the same exception raised by OAuth2PasswordBearer. 
+    # We are taking control of the authentication flow.    
+    if token == None:
+        exception = credentials_exception()
+        logger.debug(exception.detail)
+        return exception
+    
+    return decode_access_token(token)
 
 def json_req(request: Request):
     if RESPONSE_FORMAT in request.headers:
