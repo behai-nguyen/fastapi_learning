4ad0f81eef75531b74a9b2f488e5c4c8b0e7a17e
behai-nguyen <behai_nguyen@hotmail.com>
2024-11-22 12:02:04 +1100

    OAuth2 Scopes and more employees maintenance functions.
    Added tests for new methods.

--- tests/unit/test_employees.py
+++ tests/unit/test_employees.py
@@ -9,6 +9,8 @@ Use --capture=no to enable print output. I.e.:
 venv\Scripts\pytest.exe -m employees --capture=no
 """
 
+import datetime
+
 import pytest
 
 from http import HTTPStatus
@@ -94,3 +96,72 @@ def test_unit_select_by_email_not_found(app):
     assert len( status_dict['status']['text'] ) > 0
     assert ('data' not in status_dict) == True
 
+@pytest.mark.employees
+def test_unit_select_by_partial_last_name_and_first_name(app):
+    """
+    Dates are not serialised!
+    """
+    employees = Employees()
+
+    status = employees.select_by_partial_last_name_and_first_name('%nas%', '%An')
+
+    assert status.code == HTTPStatus.OK.value
+    assert len(status.text) > 0
+    assert status.data != None
+    assert len(status.data) >= 30
+
+    assert status.data[0]['emp_no'] == 12483
+    assert status.data[0]['birth_date'] == datetime.date(1959, 10, 19)
+    assert status.data[0]['first_name'] == 'Niranjan'
+    assert status.data[0]['last_name'] == 'Gornas'
+    assert status.data[0]['gender'] == 'M'
+    assert status.data[0]['hire_date'] == datetime.date(1990, 1, 10)
+
+@pytest.mark.employees
+def test_unit_select_by_employee_number(app):
+    """
+    Dates are in Australian format.
+    """
+    employees = Employees()
+
+    status = employees.select_by_employee_number(10399)
+
+    assert status.code == HTTPStatus.OK.value
+    assert len(status.text) > 0
+    assert status.data != None
+    assert len(status.data) == 1
+
+    assert status.data[0]['emp_no'] == 10399
+    assert status.data[0]['birth_date'] == '13/06/1957'
+    assert status.data[0]['first_name'] == 'Guenter'
+    assert status.data[0]['last_name'] == 'Marchegay'
+    assert status.data[0]['gender'] == 'F'
+    assert status.data[0]['hire_date'] == '07/05/1985'
+
+    status_dict = status.as_dict()
+    assert status_dict['status']['code'] == HTTPStatus.OK.value
+    assert len( status_dict['status']['text'] ) > 0
+    assert status_dict['data'][0]['emp_no'] == 10399
+    assert status_dict['data'][0]['birth_date'] == '13/06/1957'
+    assert status_dict['data'][0]['first_name'] == 'Guenter'
+    assert status_dict['data'][0]['last_name'] == 'Marchegay'
+    assert status_dict['data'][0]['gender'] == 'F'
+    assert status_dict['data'][0]['hire_date'] == '07/05/1985'
+
+@pytest.mark.employees
+def test_unit_count_by_email(app):
+    employees = Employees()
+
+    status = employees.count_by_email('heekeun.majewski.10153@gmail.com')
+
+    assert status.code == HTTPStatus.OK.value
+    assert len(status.text) > 0
+    assert status.data != None
+    assert len(status.data) == 1
+
+    assert status.data[0]['_count_'] == 1
+
+    status_dict = status.as_dict()
+    assert status_dict['status']['code'] == HTTPStatus.OK.value
+    assert len( status_dict['status']['text'] ) > 0
+    assert status_dict['data'][0]['_count_'] == 1
