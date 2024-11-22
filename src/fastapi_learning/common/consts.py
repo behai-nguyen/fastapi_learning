2040b0039db9177fa1cbfa33e46ff9c4768bae4d
behai-nguyen <behai_nguyen@hotmail.com>
2024-11-22 11:59:24 +1100

    OAuth2 Scopes and more employees maintenance functions.
    Added more text messages.

--- src/fastapi_learning/common/consts.py
+++ src/fastapi_learning/common/consts.py
@@ -7,6 +7,8 @@ RESPONSE_FORMAT = "x-expected-format"
 LOGIN_PAGE_TITLE = "Learn FastAPI Login"
 HOME_PAGE_TITLE = "Learn FastAPI Home"
 ME_PAGE_TITLE = "Learn FastAPI My Info"
+SEARCH_EMPLOYEES_PAGE_TITLE = "Learn FastAPI Search Employees"
+EMPLOYEES_MAINTENANCE_PAGE_TITLE = "Learn FastAPI Employees Maintenance"
 
 EMAIL_NO_MATCHING_MSG = 'No employee matches submitted email'
 
@@ -25,3 +27,6 @@ INVALID_PERMISSIONS_MSG = "Not enough permissions"
 ALREADY_LOGGED_IN_MSG = "Already logged in. Redirect to home page"
 
 LOG_IN_CONTINUE_MSG = "Please log in to continue"
+
+ERR_INVALID_SEARCH_EMP_NUMBER_MSG = "Invalid search employee number"
+DUPLICATE_EMAIL_MSG = "Can't accept this email. Please specify another"
