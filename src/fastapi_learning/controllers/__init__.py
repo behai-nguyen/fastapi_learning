494554f6adc4ae31948069eb572c2093eda0a5fe
behai-nguyen <behai_nguyen@hotmail.com>
2024-12-02 11:37:50 +1100

    Standardised JSON response via ResultStatus. General refactoring. Improved logic.
    Updates include:
    Removed def is_logged_in(...), def has_required_scopes(...).
    Refactored async def verify_user_scopes(...):
        - LoggedInEmployee replaced access token param.
        - the return tuple is now (bool, permissions checking result).
    Improved template codes:
        - added template method: def get_ui_states(...): replaces has_required_scopes(...).
    Added the following one-liners session helper methods:
        - def set_access_token(request: Request, token: str):
        - def get_access_token(request: Request) -> Union[str | None]:
        - def delete_access_token(request: Request):
        - def no_access_token(request: Request) -> bool:
        - set_login_redirect_code(request: Request, code: int):
        - get_login_redirect_code(request: Request, delete_also: bool=False) -> int:
        - delete_login_redirect_code(request: Request):
        - def set_login_redirect_message(request: Request, message: str):
        - def get_login_redirect_message(request: Request, delete_also: bool=False) -> str:
        - def delete_login_redirect_message(request: Request):

--- src/fastapi_learning/controllers/__init__.py
+++ src/fastapi_learning/controllers/__init__.py
@@ -28,6 +28,7 @@ from fastapi_learning.common.consts import (
     NOT_AUTHENTICATED_MSG,
     RESPONSE_FORMAT,
 )
+from fastapi_learning.models.employees import LoggedInEmployee
 from fastapi_learning.common.jwt_utils import decode_access_token
 from fastapi_learning.common.scope_utils import has_required_permissions
 
@@ -56,35 +57,45 @@ def enable_no_scopes_ui() -> bool:
 
     return str_to_bool(os.environ.get('ENABLE_NO_SCOPES_UI'))
 
-def has_required_scopes(required_scopes: list, assigned_scopes: list) -> bool:
+def get_ui_states(checked_scopes: list[str], assigned_scopes: list[str]) -> list[str]:
+    """A UI state is 'disabled' or enabled (a blank string).
+
+    :param list[str] checked_scopes: Input. A list of scopes whose UI states are to
+        be determined.
+
+    :param list[str] assigned_scopes: Input. Scopes which are assigned to the 
+        logged-in user 
+
+    :return: list of string whose values are 'disabled' or blank. The total number of 
+        elements equal to the length of :param list[str] checked_scopes:.
     """
-    Template method. 
-    Check if all scopes in param assigned_scopes are in param 
-    required_scopes.
-    """
-    return has_required_permissions(required_scopes, assigned_scopes)
+
+    if enable_no_scopes_ui(): 
+        return ['' for x in range(len(checked_scopes))]
+    
+    res: list[str] = []
+    for s in checked_scopes:
+        res.append('') if has_required_permissions([s], assigned_scopes) else res.append('disabled')
+
+    return res
 
 async def verify_user_scopes(security_scopes: SecurityScopes, 
-                             token: str, failed_msg: str) -> tuple:
+                             logged_in_user: LoggedInEmployee, 
+                             failed_msg: str) -> tuple:
     """
     Check if users have scopes given the required scopes and user JWT.
 
     Return: 
-        (bool, decoded token data, permissions checking result)
-        (bool, [TokenData | HTTPException], [HTTPException | None])
+        (bool, permissions checking result)
+        (bool, [HTTPException | None])
 
         Only possible return values:
 
-        (False, HTTPException, None)
-        (False, TokenData, HTTPException)
-        (True, TokenData, None)
+        (False, HTTPException)
+        (True, None)
     """
 
-    token_data = attempt_decoding_access_token(token)
-    if isinstance(token_data, HTTPException):
-        return (False, token_data, None)
-    
-    if not has_required_permissions(security_scopes.scopes, token_data.scopes): 
+    if not has_required_permissions(security_scopes.scopes, logged_in_user.scopes): 
         logger.debug(failed_msg)
 
         if security_scopes.scopes:
@@ -95,16 +106,16 @@ async def verify_user_scopes(security_scopes: SecurityScopes,
         exception = credentials_exception(authenticate_value=authenticate_value)
         exception.detail = failed_msg
 
-        return (False, token_data, exception)
+        return (False, exception)
     
-    return (True, token_data, None)
+    return (True, None)
 
 templates = Jinja2Templates(directory="src/fastapi_learning/templates")
 # Added functions to be used by templates.
 # 
 templates.env.globals['valid_logged_in_employee'] = valid_logged_in_employee
 templates.env.globals['enable_no_scopes_ui'] = enable_no_scopes_ui
-templates.env.globals['has_required_scopes'] = has_required_scopes
+templates.env.globals['get_ui_states'] = get_ui_states
 
 def credentials_exception(detail: str=NOT_AUTHENTICATED_MSG, 
                           authenticate_value: str="Bearer") -> HTTPException:
@@ -146,11 +157,39 @@ async def html_req(request: Request):
     form_data = await request.form()
     return form_data.get(RESPONSE_FORMAT) == types_map['.html']
 
-def is_logged_in(request: Request) -> bool:
-    """
-    Is the current session authenticated?
-    """
-    return request.session.get("access_token") != None
+def set_access_token(request: Request, token: str):
+    request.session["access_token"] = token
+
+def get_access_token(request: Request) -> Union[str | None]:
+    return request.session.get("access_token")
+
+def delete_access_token(request: Request):
+    request.session.pop("access_token", None)
+
+def no_access_token(request: Request) -> bool:
+    return request.session.get("access_token") == None
+
+def set_login_redirect_code(request: Request, code: int):
+    request.session["login_redirect_code"] = code
+
+def get_login_redirect_code(request: Request, delete_also: bool=False) -> int:
+    code: int = int(request.session.get("login_redirect_code", status.HTTP_200_OK))
+    if delete_also: request.session.pop("login_redirect_code", None)
+    return code
+
+def delete_login_redirect_code(request: Request):
+    request.session.pop("login_redirect_code", None)
+
+def set_login_redirect_message(request: Request, message: str):
+    request.session["login_redirect_msg"] = message
+
+def get_login_redirect_message(request: Request, delete_also: bool=False) -> str:
+    msg: str = request.session.get("login_redirect_msg", "")
+    if delete_also: request.session.pop("login_redirect_msg", None)
+    return msg
+
+def delete_login_redirect_message(request: Request):
+    request.session.pop("login_redirect_msg", None)
 
 class JsonAPIRoute(APIRoute):
     """
