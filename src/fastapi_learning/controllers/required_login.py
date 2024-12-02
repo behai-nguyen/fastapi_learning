7e1a43a33e788317ac24d1529292cd68a4587eed
behai-nguyen <behai_nguyen@hotmail.com>
2024-12-02 11:37:06 +1100

    Standardised JSON response via ResultStatus. General refactoring. Improved logic.
    Updated async def get_logged_in_user(...):
        - when failed decoded token raises RequiresLogin with token_data.detail.
        - copies token payload scopes over to the returned LoggedInEmployee.
        - Cache the returned LoggedInEmployee to session as JSON under the key logged_in_user.
    Added async def get_cached_logged_in_user(request: Request) -> LoggedInEmployee:
    Refactored class RequiresLogin(HTTPException): sub-class of HTTPException.

--- src/fastapi_learning/controllers/required_login.py
+++ src/fastapi_learning/controllers/required_login.py
@@ -8,7 +8,7 @@
 
 from typing import Annotated, Union
 
-from fastapi import HTTPException, Depends, status
+from fastapi import HTTPException, Depends, status, Request
 
 from fastapi_learning import oauth2_scheme
 
@@ -17,18 +17,16 @@ from fastapi_learning.models.employees import LoggedInEmployee
 
 from fastapi_learning.controllers import attempt_decoding_access_token
 
-from fastapi_learning.common.consts import (
-    NOT_AUTHENTICATED_MSG,
-    INVALID_AUTH_CREDENTIALS_MSG,
-)
+from fastapi_learning.common.consts import INVALID_AUTH_CREDENTIALS_MSG
 
-class RequiresLogin(Exception):
+class RequiresLogin(HTTPException):
     """
     Exception to redirect to login route.
     """
     pass
 
-async def get_logged_in_user(token: Annotated[str, Depends(oauth2_scheme)]) \
+async def get_logged_in_user(request: Request,
+                             token: Annotated[str, Depends(oauth2_scheme)]) \
     -> Union[HTTPException | LoggedInEmployee]:
 
     """
@@ -38,11 +36,26 @@ async def get_logged_in_user(token: Annotated[str, Depends(oauth2_scheme)]) \
     token_data = attempt_decoding_access_token(token)
 
     if isinstance(token_data, HTTPException):
-        raise RequiresLogin(NOT_AUTHENTICATED_MSG)
+        raise RequiresLogin(token_data.status_code, token_data.detail)
     
     op_status = EmployeesManager().select_by_email(token_data.user_name)
 
     if op_status.code != status.HTTP_200_OK:
-        raise RequiresLogin(INVALID_AUTH_CREDENTIALS_MSG)
+        raise RequiresLogin(op_status.code, INVALID_AUTH_CREDENTIALS_MSG)
+    
+    # To be retrieved by method async def get_cached_logged_in_user(...).
+    logged_in_user = LoggedInEmployee(**op_status.data[0], scopes=token_data.scopes)
+    request.session["logged_in_user"] = logged_in_user.model_dump_json()
+    
+    return logged_in_user
+    
+async def get_cached_logged_in_user(request: Request) -> LoggedInEmployee:
+    """
+    Retrieve the cached logged-in user in session stored by get_logged_in_user(...).
+    The intention is this method is called in the same request after verifying the 
+    session is authenticated and not expired.
+    """
 
-    return LoggedInEmployee(**op_status.data[0])
+    import json
+
+    return LoggedInEmployee( **json.loads(request.session["logged_in_user"]) )
