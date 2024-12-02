00b4a1dce713b9e1b898f4eabaf12a576cbf4cb4
behai-nguyen <behai_nguyen@hotmail.com>
2024-12-02 11:38:24 +1100

    Standardised JSON response via ResultStatus. General refactoring. Improved logic.
    Updates include:
    All JSON responses are returned via ResultStatus.
    async def read_users_me(...):
        - code cleaned up.
        - no longer check for authenticated session.
        - for JSON response, now returns ResultStatus.
    async def get_current_user(...):
        - uses [ user = Depends(get_logged_in_user), ]
        - uses verify_user_scopes(...)

--- src/fastapi_learning/controllers/admin.py
+++ src/fastapi_learning/controllers/admin.py
@@ -2,43 +2,35 @@
 14/05/2024.
 """
 
-from typing import Annotated
+from typing import Annotated, Union
 
 from fastapi import (
     APIRouter, 
     Depends,     
-    HTTPException, 
     Security,
-    status,
     Request,
-    status,
+    HTTPException,
 )
 
 from fastapi.security import SecurityScopes
 
-from fastapi_learning import oauth2_scheme
-
-from fastapi.responses import RedirectResponse
+from bh_apistatus.result_status import make_status
 
 from fastapi_learning.controllers import (
-    credentials_exception,
-    attempt_decoding_access_token,
-    is_logged_in,
+    verify_user_scopes,
     templates,
 )
 
 from fastapi_learning.models.employees import LoggedInEmployee
 
-from fastapi_learning.businesses.employees_mgr import EmployeesManager
-
 from fastapi_learning.common.consts import (
-    INVALID_AUTH_CREDENTIALS_MSG,
     ME_PAGE_TITLE,
     INVALID_PERMISSIONS_MSG,
 )
 
 from fastapi_learning.common.queue_logging import logger
-from fastapi_learning.common.scope_utils import has_required_permissions
+
+from fastapi_learning.controllers.required_login import get_logged_in_user
 
 from . import json_req, JsonAPIRoute
 
@@ -56,38 +48,27 @@ api_router = APIRouter(route_class=JsonAPIRoute,
 
 async def get_current_user(
         security_scopes: SecurityScopes,
-        token: Annotated[str, Depends(oauth2_scheme)]):
-    if security_scopes.scopes:
-        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
-    else:
-        authenticate_value = "Bearer"
+        user = Depends(get_logged_in_user),
+) -> Union[HTTPException | LoggedInEmployee]:
+    """
+    Current user is the logged in user.
 
-    creds_exception = credentials_exception(authenticate_value=authenticate_value)
-    
-    token_data = attempt_decoding_access_token(token)
-    
-    if isinstance(token_data, HTTPException):
-        return token_data
+    get_logged_in_user() is called to verify an authenticated session and 
+    retrieve the logged-in user. Then based on the logged-in user's scopes,
+    verify that they have sufficient permissions to view their own details, 
+    otherwise return HTTPException with appropriate error.
+    """
 
-    op_status = EmployeesManager().select_by_email(token_data.user_name)
-
-    # Enables this to simulate token became invalid after logged in.
-    # op_status = EmployeesManager().select_by_email('token@gmail.com')
-    if op_status.code != status.HTTP_200_OK:
-        logger.debug(INVALID_AUTH_CREDENTIALS_MSG)
-        creds_exception.detail = INVALID_AUTH_CREDENTIALS_MSG
-        return creds_exception
-
-    if not has_required_permissions(security_scopes.scopes, token_data.scopes): 
+    res, exception = await verify_user_scopes(security_scopes, user, INVALID_PERMISSIONS_MSG)
+    if not res: 
         logger.debug(INVALID_PERMISSIONS_MSG)
-        creds_exception.detail = INVALID_PERMISSIONS_MSG
-        return creds_exception
+        return exception
 
-    return LoggedInEmployee(**op_status.data[0])
+    return user
 
 @router.get("/me")
 async def read_users_me(
-    request: Request,
+    request: Request, 
     current_user: Annotated[LoggedInEmployee, 
                             Security(get_current_user, scopes=["user:read"])]
 ):
@@ -107,13 +88,8 @@ async def read_users_me(
     - Invalid credentials: The requests contain an ``Authorization`` 
       header with an invalid token, such as ``Bearer behai_``.
 
-    When an error occurs, the return format is:
-
-    - ``JSON``: The response ``status_code`` is ``HTTP_401_UNAUTHORIZED``. 
-      The ``detail`` field value is a text message.
-
-    - ``HTML``: The response ``status_code`` is ``HTTP_200_OK``. The 
-      HTML text is the login page with the message ``LOG_IN_CONTINUE_MSG``.
+    See ./tests/integration/test_admin_itgt.py for more detail on possible 
+    responses.
     """
 
     def page_me():
@@ -122,27 +98,21 @@ async def read_users_me(
 
         return templates.TemplateResponse(request=request, name="admin/me.html", 
                 context={'data': user_dict})
-        
+
     if isinstance(current_user, LoggedInEmployee):
         logger.debug('Returning a valid logged-in user.')
 
-        return current_user if json_req(request) else page_me()
-    else:
-        logger.debug('No valid logged-in user found.')
+        return make_status().add_data(vars(current_user)).as_dict() \
+            if json_req(request) else page_me()
 
-        if json_req(request): 
-            raise current_user
+    else:
+        logger.debug(current_user.detail)
+
+        if json_req(request):
+            return make_status(code=current_user.status_code, text=current_user.detail).as_dict()
         else: 
-            if is_logged_in(request):                
-                # This error:
-                # {'status_code': 401, 'detail': 'Invalid authentication credentials', 'headers': {'WWW-Authenticate': 'Bearer'}}
-                # should not happen. It happens if the email in the database changed while                 
-                # user logged in.
-                #
-                return templates.TemplateResponse(request=request, name="admin/me.html", 
-                        context={'data': vars(current_user)})
-            else:
-                return RedirectResponse(url='/auth/login?state=2')
+            return templates.TemplateResponse(request=request, name="admin/me.html", 
+                    context={'data': vars(current_user)})
 
 #=================================== /api ==================================
 
