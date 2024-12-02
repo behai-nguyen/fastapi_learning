e1259e4dea2a93ad34c71be291e9e3f9540dc547
behai-nguyen <behai_nguyen@hotmail.com>
2024-12-02 11:38:48 +1100

    Standardised JSON response via ResultStatus. General refactoring. Improved logic.
    Updates include:
    All JSON responses are returned via ResultStatus.
    Removed def __login_page_context(...).
    Completely rewrote async def login_page(...).
    New def __login_response(...) replaces def __login_page(...).
    Refactored async def home_page(...):
        - uses [user = Depends(get_logged_in_user)]
    Removed helper method def __home_page(...).
    Refactored async def login(...) -- check for access token exists, if it does then call get_logged_in_user(...).
    Refactored async def logout(request: Request):
        - stores redirect message in session under the key login_redirect_msg.
        - redirects state is now LOGIN_REDIRECT_STATE_CERTAIN.

--- src/fastapi_learning/controllers/auth.py
+++ src/fastapi_learning/controllers/auth.py
@@ -2,10 +2,7 @@
 14/05/2024.
 """
 
-from typing import (
-    Annotated,
-    Union,
-)
+from typing import Annotated
 
 from fastapi import (
     APIRouter, 
@@ -18,36 +15,50 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import (
     HTMLResponse,
     RedirectResponse,
+    JSONResponse,
 )
 
-from fastapi_learning import oauth2_scheme
-
-from bh_apistatus.result_status import ResultStatus
+from bh_apistatus.result_status import (
+    ResultStatus, 
+    make_status,
+)
 
 from fastapi_learning import Token
 
+from fastapi_learning.models.employees import LoggedInEmployee
 from fastapi_learning.businesses.employees_mgr import EmployeesManager
 
 from fastapi_learning.controllers import (
-    attempt_decoding_access_token,
-    is_logged_in,
     templates,
+    json_req,
+    set_access_token,
+    get_access_token,
+    delete_access_token,
+    no_access_token,
+    set_login_redirect_code,
+    get_login_redirect_code,
+    delete_login_redirect_code,
+    set_login_redirect_message,
+    get_login_redirect_message,
+    delete_login_redirect_message,
 )
 
 from fastapi_learning.common.jwt_utils import create_access_token
 
 from fastapi_learning.common.consts import (
+    LOGIN_REDIRECT_STATE_CERTAIN,
     LOGIN_PAGE_TITLE,
     HOME_PAGE_TITLE,
     LOGIN_ERROR_MSG,
     BAD_LOGIN_MSG,
     INVALID_USERNAME_PASSWORD_MSG,
     NOT_LOGGED_IN_SESSION_MSG,
-    LOG_IN_CONTINUE_MSG,
     LOGGED_IN_SESSION_MSG,
 )
 
 from fastapi_learning.common.queue_logging import logger
+
+from fastapi_learning.controllers.required_login import get_logged_in_user
 
 from . import (
     html_req, 
@@ -66,63 +77,73 @@ api_router = APIRouter(route_class=JsonAPIRoute,
     tags=["API"],
 )
 
-def __login_page_context(state: int) -> dict:
-    context = {"title": LOGIN_PAGE_TITLE}
-    match state:
-        case 1: context.update({"message": NOT_LOGGED_IN_SESSION_MSG})
-        case 2: context.update({"message": LOG_IN_CONTINUE_MSG})
-        # case 3: context.update({"message": INVALID_USERNAME_PASSWORD_MSG})
-        case status.HTTP_401_UNAUTHORIZED: context.update({"message": INVALID_USERNAME_PASSWORD_MSG})
-        case status.HTTP_500_INTERNAL_SERVER_ERROR: context.update({"message": BAD_LOGIN_MSG})
-        case _: pass
+def __login_response(request: Request) -> HTMLResponse: 
+    logger.debug('Delivering the login response.')
 
-    return context
+    message = get_login_redirect_message(request, delete_also=True)
 
-def __login_page(request: Request, state: int=0) -> HTMLResponse: 
-    logger.debug('Delivering the login page.')
+    # The login page request is ALWAYS HTML.
+    # But there is only a single login redirect route, other JSON requests 
+    # which require authenticated session also get redirected here when 
+    # their sessions are not authenticated, hence has to respond in JSON.
+    if json_req(request):
+        return JSONResponse(make_status(code=get_login_redirect_code(request, delete_also=True), 
+                                        text=message).as_dict())
 
     return templates.TemplateResponse(request=request, 
-                name="auth/login.html", context=__login_page_context(state))
+                name="auth/login.html", 
+                context={"title": LOGIN_PAGE_TITLE, 
+                         "message": message})
 
-def __home_page(request: Request,
-                token: Annotated[str, Depends(oauth2_scheme)]) -> HTMLResponse: 
-    logger.debug('Delivering the home page.')
-
-    token_data = attempt_decoding_access_token(token)
-
-    data = {}
-    if isinstance(token_data, HTTPException):
-        data.update({"status_code": token_data.status_code, "detail": token_data.detail})
-    else:
-        data.update({"user_number": token_data.user_number, "user_scopes": token_data.scopes})
-
-    # data.update({"status_code": 401, "detail": "Failed test..."})
-
-    return templates.TemplateResponse(request=request, name="auth/home.html", 
-                                      context={"title": HOME_PAGE_TITLE, "data": data})
-
-@router.get("/login", response_model=None)
+@router.get("/login")
 async def login_page(request: Request, state: int = 0) -> HTMLResponse:
-    logger.debug('Attempt to deliver the login page.')
+    """
+    HTML response: Return the login page.
+    """
 
-    return RedirectResponse(url=router.url_path_for('home_page')) \
-        if is_logged_in(request) \
-        else __login_page(request=request, state=state)
+    logger.debug(f'Attempt to deliver the login page, state: {state}.')
 
-@router.get("/home", response_model=None)
+    if state == LOGIN_REDIRECT_STATE_CERTAIN:
+        delete_access_token(request)
+        return __login_response(request=request)
+
+    # Prepares to check if this is an authenticated session.
+    # access_token is None is not an error condition: user has not 
+    # logged in before, just deliver the login HTML page.
+    access_token = get_access_token(request)
+    if access_token == None:
+        return __login_response(request=request)
+    
+    data = await get_logged_in_user(request, access_token)
+    if isinstance(data, HTTPException):
+        delete_access_token(request)
+        set_login_redirect_code(request, data.status_code)
+        set_login_redirect_message(request, str(data))
+        return __login_response(request=request)
+    
+    return RedirectResponse(url=router.url_path_for('home_page'))
+
+@router.get("/home")
 async def home_page(request: Request,
-                    token: Annotated[str, Depends(oauth2_scheme)]) -> HTMLResponse:
+                    user = Depends(get_logged_in_user)) -> HTMLResponse:
+    """
+    HTML response: Return the home page.
+    """
+
     logger.debug('Attempt to deliver the home page.')
 
-    return RedirectResponse(url=f"{router.url_path_for('login_page')}?state=2") \
-        if not is_logged_in(request) \
-        else __home_page(request=request, token=token)
+    return templates.TemplateResponse(request=request, name="auth/home.html", 
+                                      context={"title": HOME_PAGE_TITLE, 
+                                               "data": {"user_number": user.emp_no, 
+                                                        "user_scopes": user.scopes}})
 
 @router.post("/token")
 async def login(request: Request,
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Union[Token, None]:
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
     """
+    Authenticate users.
+
     The response from this method can be in either ``JSON`` or 
     ``HTML`` format.
 
@@ -138,21 +159,8 @@ async def login(request: Request,
     - ``JSON``: Returns the following JSON: 
       ``{"access_token": <token>, "token_type": "bearer"}``.
 
-    There are two possible failure conditions:
-
-    - Invalid username: The submitted username does not match any in 
-      the database.
-
-    - Invalid password: The submitted password does not match the password 
-      in the database for the given username.
-
-    When login fails, depending on the requested return format:
-
-    - ``HTML``: The response ``status_code`` is ``HTTP_200_OK``. The 
-      HTML text is the login page with the message ``INVALID_USERNAME_PASSWORD_MSG``.
-    
-    - ``JSON``: The response ``status_code`` is ``HTTP_400_BAD_REQUEST``. 
-      The ``detail`` field value is ``INVALID_USERNAME_PASSWORD_MSG``.    
+    See ./tests/integration/test_auth_itgt.py for more details on possible 
+    responses.
     """
 
     logger.debug('Attempt to log in...')
@@ -165,29 +173,31 @@ async def login(request: Request,
             case status.HTTP_500_INTERNAL_SERVER_ERROR: message = BAD_LOGIN_MSG
             case _: message = LOGIN_ERROR_MSG
 
+        set_login_redirect_code(request, op_status.code)
+        set_login_redirect_message(request, message)
+
         if await html_req(request):
-            return RedirectResponse(url=f"{router.url_path_for('login_page')}?state={op_status.code}", 
+            return RedirectResponse(url=f"{router.url_path_for('login_page')}?state={LOGIN_REDIRECT_STATE_CERTAIN}", 
                                     status_code=status.HTTP_303_SEE_OTHER)        
         else:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
-                                detail=message)
+            # return make_status(code=status.HTTP_400_BAD_REQUEST, text=message).as_dict()
+            return make_status(code=op_status.code, text=message).as_dict()
 
-    if is_logged_in(request): 
-        logger.debug(LOGGED_IN_SESSION_MSG)
+    # Check if the session is authenticated and is still valid.
+    access_token = get_access_token(request)
+    if access_token != None:
+        data = await get_logged_in_user(request, access_token)
 
-        return RedirectResponse(url=router.url_path_for('home_page'), status_code=status.HTTP_303_SEE_OTHER) \
-            if await html_req(request) else \
-                Token(access_token=request.session["access_token"], token_type="bearer", detail=LOGGED_IN_SESSION_MSG)
-
+        if isinstance(data, LoggedInEmployee):
+            return RedirectResponse(url=router.url_path_for('home_page'), status_code=status.HTTP_303_SEE_OTHER) \
+                if await html_req(request) else \
+                    make_status().add_data(
+                        vars(Token(access_token=request.session["access_token"], 
+                                   token_type="bearer", detail=LOGGED_IN_SESSION_MSG))
+                    ).as_dict()
+    
     op_status = EmployeesManager().login(form_data.username, form_data.password)
 
-    """
-    from bh_utils.json_funcs import dumps
-    f = open( 'op_status.json', 'w' )
-    f.write( dumps(op_status.as_dict()) )
-    f.close()
-    """
-    
     if op_status.code != status.HTTP_200_OK:
         return await bad_login(op_status)
     
@@ -195,34 +205,31 @@ async def login(request: Request,
                                              'emp_no': op_status.data[0]['emp_no'],
                                              'scopes': op_status.data.scopes})
 
-    request.session["access_token"] = access_token
+    set_access_token(request, access_token)
 
     return RedirectResponse(url=router.url_path_for('home_page'), status_code=status.HTTP_303_SEE_OTHER) \
-        if await html_req(request) else Token(access_token=access_token, token_type="bearer", detail="")
+        if await html_req(request) else \
+            make_status().add_data(
+                vars(Token(access_token=access_token, token_type="bearer", detail=""))
+            ).as_dict()
 
 @router.post("/logout", response_class=HTMLResponse)
 async def logout(request: Request):
     """
     Logs out and redirects to the login route ``/auth/login``.
-
-    There are two possible error conditions:
-
-    - Not logged in: The users have not yet authenticated.
-
-    - Invalid credentials: The requests contain an ``Authorization`` 
-      header with an invalid token, such as ``Bearer behai_``.
-
-    When an error occurs, the returned login page will contain the text 
-    ``NOT_LOGGED_IN_SESSION_MSG``.
     """
+
+    def __clean_session(request: Request):
+        delete_login_redirect_code(request)
+        delete_login_redirect_message(request)
+        delete_access_token(request)
 
     logger.debug('Attempt to log out...')
 
-    inc_not_login_msg = 1 if request.session.get("access_token") == None else 0
-    
-    request.session.clear()
-    
-    return RedirectResponse(url=f"{router.url_path_for('login_page')}?state={inc_not_login_msg}",
+    set_login_redirect_message(request, NOT_LOGGED_IN_SESSION_MSG) \
+        if no_access_token(request) else __clean_session(request)
+
+    return RedirectResponse(url=f"{router.url_path_for('login_page')}?state={LOGIN_REDIRECT_STATE_CERTAIN}",
                             status_code=status.HTTP_303_SEE_OTHER)
 
 #=================================== /api ==================================
